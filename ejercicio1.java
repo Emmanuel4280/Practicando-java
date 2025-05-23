2f5ac5365912a602cd15f32d0fda586dba337018
Emmanuel4280 <22000118@galileo.edu>
2025-05-22 23:26:32 -0600

    'practicando'

---
+++ ejercicio1.java
@@ -0,0 +1,18 @@
+
+
+public class ejercicio1{
+
+    public static int a = 10;
+    public static int b = 10;
+
+    public static int suma(){
+
+        return a + b;
+    }
+
+
+    public static void main(String[] args){
+
+        System.out.println( suma());
+    }
+    }
